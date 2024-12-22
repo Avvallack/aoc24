6b77766d3a28125ead57964505a3109ddabb943b
Sergei Dudoladov <sergei.dudoladov@jetbrains.com>
2024-12-22 11:06:36 +0100

    day 19

---
+++ src/day19.rs
@@ -0,0 +1,105 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[derive(Clone, Debug)]
+pub struct Input {
+    tokens: Vec<String>,
+    patterns: Vec<String>,
+}
+
+#[aoc_generator(day19)]
+fn read_inputs(input: &str) -> Option<Input> {
+    let mut iter = input.split("\n\n");
+    let first = iter.next().unwrap().split(", ").map(|s| s.to_string()).collect();
+    let second = iter.next().unwrap().lines().map(|x| x.to_string()).collect();
+    Some(Input{tokens: first, 
+            patterns: second})
+}
+
+fn count_tokenization(text: &str, patterns: &Vec<String>) -> u64 {
+    let n = text.len();
+    let mut dp = vec![0; n + 1];
+    dp[0] = 1;
+
+    for i in 1..n + 1 {
+        for j in 0..=i {
+            if dp[j] > 0 && patterns.contains(&text[j..i].to_string()) {
+                dp[i] += dp[j];
+
+            }
+        }
+    }
+    dp[n]
+}
+
+fn can_tokenize(text: &str, patterns: &Vec<String>) -> bool {
+    let n = text.len();
+    let mut dp = vec![false; n + 1];
+    dp[0] = true;
+
+    for i in 1..n + 1 {
+        for j in 0..=i {
+            if dp[j] && patterns.contains(&text[j..i].to_string()) {
+                dp[i] = true;
+            }
+        }
+    }
+    dp[n]
+}
+
+#[aoc(day19, part1)]
+fn part_1 (inp: &Input) -> u64 {
+    let mut possible  = Vec::new();
+    for stripe in &inp.patterns {
+        if can_tokenize(&stripe, &inp.tokens) {
+            possible.push(stripe);
+        }
+    }
+
+    possible.len() as u64
+}
+
+#[aoc(day19, part2)]
+fn part_2 (inp: &Input) -> u64 {
+    let mut possible  = Vec::new();
+    for stripe in &inp.patterns {
+        if can_tokenize(&stripe, &inp.tokens) {
+            possible.push(stripe);
+        }
+    }
+    let mut sums = Vec::new();
+    for towel in possible {
+        let sum = count_tokenization(&towel, &inp.patterns);
+        sums.push(sum);
+    }
+    sums.iter().sum::<u64>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "r, wr, b, g, bwu, rb, gb, br\n\
+                              \n\
+                              brwrr\n\
+                              bggr\n\
+                              gbbr\n\
+                              rrbgbr\n\
+                              ubwu\n\
+                              bwurrg\n\
+                              brgr\n\
+                              bbrgwb";
+
+    #[test]
+    fn test_p1() {
+        let inp = read_inputs(TEST_INPUT).expect("valid input");
+        let res = part_1(&inp);
+        assert_eq!(res, 6);
+    }
+
+    #[test]
+    fn test_p2() {
+        let inp  = read_inputs(TEST_INPUT).expect("valid input");
+        let res = part_2(&inp);
+        assert_eq!(res, 16);
+    }
+}
---
+++ src/day21.rs
@@ -0,0 +1,29 @@
+// use aoc_runner_derive::{aoc, aoc_generator};
+
+// #[cfg(test)]
+// mod tests{
+//     const TEST_INPUT: &str = r#"029A
+// 980A
+// 179A
+// 456A
+// 379A
+// "#;
+
+// const RES1: String = "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A".to_string();
+// const RES2: String = "<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A".to_string();
+// const RES3: String = "<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A".to_string();
+// const RES4: String = "<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A".to_string();
+// const RES5: String = "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A".to_string();
+
+//     #[test]
+//     fn test_part1() {
+//         let inp = parse_input(TEST_INPUT);
+//         let p1 = inp[0];
+//         assert_eq!(part1(&p1), RES1);
+//     }
+
+//     #[test]
+//     fn test_part2() {
+//         assert_eq!(part2(&generator(TEST_INPUT)), 5);
+//     }
+// }
